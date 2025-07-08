9567752f9fb836f9c30f9417cdd04748f38a12dc
Sea <sea@192.168.0.108>
2025-07-09 01:33:09 +0800

    second push

--- class.py
+++ class.py
@@ -2,7 +2,7 @@ class Student:
     def __init__(self, name, studentID, score):
         self.name = name
         self.studentID = studentID
-        self.score = score
+        self.score = score++++++++++++++
 
 student = Student('zhang san',1,{'math':90,'english':80,'chinese':98})
 
