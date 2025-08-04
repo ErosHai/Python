01d5cd2c6511eee84c89990e50d115a86ee2c8dc
Hai <zhl826882143@gmail.com>
2025-08-04 15:41:55 +0800

    练习题目总结

--- Exercises.py
+++ Exercises.py
@@ -4,7 +4,11 @@ with open('./file.txt','r+',encoding='utf-8') as f:
 
 
 # 字典
-car = {'name':'BMW','model':'X5','price': 80000}
+cars = {'name':'BMW','model':'X5','price': 80000}
 
-print(len(car))
-print(car['price'])
+print(len(cars))
+print(cars['price'])
+
+for key, value, in cars.items():
+    print(f'{key} : {value}')
+
