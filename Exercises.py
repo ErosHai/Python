396b98081ec6d93838830bbf6ec2532b7ccd7d7f
Hai <zhl826882143@gmail.com>
2025-08-05 13:30:20 +0800

    练习题目总结

--- Exercises.py
+++ Exercises.py
@@ -4,7 +4,7 @@ with open('./file.txt','r+',encoding='utf-8') as f:
 
 
 # 字典
-cars = {'name':'BMW','model':'X5','price': 80000}
+cars = {'name':'BMW','model':'X5','price': 800000}
 
 print(len(cars))
 print(cars['price'])
@@ -12,3 +12,11 @@ print(cars['price'])
 for key, value, in cars.items():
     print(f'{key} : {value}')
 
+
+# 列表去重后排序，找到第二大的数
+arrs = [13,8,4,8,6,10,13]
+
+Unique_lists = sorted(set(arrs))
+
+print(Unique_lists)
+print(Unique_lists[-2])
