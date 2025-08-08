33405702787ff688519afc127134b2ab166e44d0
Hai <zhl826882143@gmail.com>
2025-08-08 14:50:30 +0800

    spider douban top250

--- get_douban.py
+++ get_douban.py
@@ -6,8 +6,17 @@ headers = {
     'user-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36'
 }
 
-response = requests.get('https://movie.douban.com/top250',headers=headers).text
+top_num = 0
 
-soup = BeautifulSoup(response,'html-parser')
+for start_num in range(0,250,25):
+    response = requests.get(f'https://movie.douban.com/top250?start={start_num}',headers=headers).text
 
-print(soup)
+    soup = BeautifulSoup(response,'html.parser')
+
+    all_title = soup.find_all('span',attrs={'class':'title'})
+
+    for title in all_title:
+        chinese_title = title.string
+        if '/' not in chinese_title:
+            top_num += 1
+            print(f'Top {top_num} -《{chinese_title}》' )
--- spider_books.py
+++ spider_books.py
@@ -5,10 +5,16 @@ import requests
 # 请求获取到网页html
 content = requests.get('https://books.toscrape.com/').text
 
-# 美化html 并提取想要的内容
+# 解析html 并提取想要的内容
 soup = BeautifulSoup(content,'html.parser')
 
+# 通过类名找到对应书价格
 allPrice = soup.find_all('p',attrs={'class': 'price_color'})
 
 for price in allPrice:
     print(price.string[2:])
+
+# 通过层层遍历找到对应书名
+allTitle = soup.find_all('h3')
+for title in allTitle:
+     print(title.find('a').string)
