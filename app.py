aa6ddb38d5803da61106ecc315304f1e96c1fa7c
Hai <zhl826882143@gmail.com>
2025-08-10 02:19:52 +0800

    learn flask

--- app.py
+++ app.py
@@ -14,9 +14,14 @@ def home_page():
 
 # 获取路径的动态参数
 @app.route('/data/appInfo/<name>',methods=['GET'])
-def query(name):
+def query_data_by_name(name):
     print(type(name))
     return 'string: => {}'.format(name)
+
+@app.route('/data/appInfo/<int:uid>',methods=['GET'])
+def query_data_by_id(uid):
+    print(type(uid))
+    return 'int: => {}'.format(uid)
 
 if __name__ == '__main__':
     app.run()
